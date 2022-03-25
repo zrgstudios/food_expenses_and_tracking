5f39faccfbc4575714808e752db087fdc8a69a85
Sean <scmwhr83@gmail.com>
2022-03-24 22:42:02 -0400

    Fixed spacing attempt #2 by switching mixed to spaces

--- DotEnv.cs
+++ DotEnv.cs
@@ -3,22 +3,22 @@ using System.IO;
 
 public static class DotEnv
 {
-	public static void Load(string file_path)
-	{	
-		if (!File.Exists(file_path))
+    public static void Load(string file_path)
+    {	
+        if (!File.Exists(file_path))
         {
-			Console.WriteLine("Couldn't find file");
-			return;
-		}
-			
-		foreach (var line in File.ReadAllLines(file_path))
-        {
-			var parts = line.Split("=", StringSplitOptions.RemoveEmptyEntries);
-			if (parts.Length != 2)
-				continue;
-
-			Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            Console.WriteLine("Couldn't find file");
+            return;
         }
-	}
+            
+        foreach (var line in File.ReadAllLines(file_path))
+        {
+            var parts = line.Split("=", StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+                continue;
+
+            Environment.SetEnvironmentVariable(parts[0], parts[1]);
+        }
+    }
 }
 
